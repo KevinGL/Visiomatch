889695fa9fb7094b26c77d611f1de0faa55e5bcf
kevin <kevin.progc@gmail.com>
2024-10-30 20:25:34 +0100

    Variables d'environnement pour accès BDD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,24 @@
-// lib/firebaseAdmin.ts
 import * as admin from 'firebase-admin';
 import path from 'path';
 import * as fs from 'fs';
 
-// Chemin vers le fichier de compte de service
 const serviceAccountPath = path.resolve("./visiomatch-cf5e1-firebase-adminsdk-uaysq-b337d09def.json");
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
+/*const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
-// Initialiser Firebase Admin uniquement si cela n'a pas déjà été fait
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    //databaseURL: process.env.FIREBASE_DATABASE_URL, // Remplacer par ton URL
+  });
+}*/
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Remplacer les nouvelles lignes si nécessaire
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    // databaseURL: process.env.FIREBASE_DATABASE_URL, // Si nécessaire
   });
 }
 
