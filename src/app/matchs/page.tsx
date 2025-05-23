b6c6e3a80885e787e47805daf48eb78b85d92274
kevin <kevin.progc@gmail.com>
2025-05-23 02:23:50 +0200

    Affichage matchs et gestion photos de profil

--- src/app/matchs/page.tsx
+++ src/app/matchs/page.tsx
@@ -4,7 +4,10 @@ import { useEffect, useState } from "react"
 import Navbar from "../components/navbar"
 import { getMatchs } from "../actions/meetings/get";
 import { Button } from "@/components/ui/button";
-import { CalendarDays, Globe, MapPin, User } from "lucide-react";
+import { CalendarDays, Globe, MapPin, User, Users } from "lucide-react";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from 'next/image';
+import { CldImage } from 'next-cloudinary';
 
 export default function Matchs()
 {
@@ -40,41 +43,97 @@ export default function Matchs()
         <>
             <Navbar />
 
+            <div className="container mx-auto p-4">
+                <h2 className="text-2xl font-bold text-pink-600 mb-6">Vos matchs</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+            
             {
-                matchs.map((m) =>
+                matchs.map((match, index: number) =>
                 {
                     return(                    
-                        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                            <div className="p-6">
-                                <div className="mb-6 p-4 bg-pink-50 rounded-lg">
-                                    <div className="grid grid-cols-2 gap-4">
-                                    <div className="flex items-center">
-                                        <User className="w-5 h-5 text-pink-600 mr-2" />
-                                        <span className="font-semibold">Pseudo:</span>
-                                        <span className="ml-2">{m.name}</span>
+                        <Card key={index} className="overflow-hidden hover:shadow-lg transition-shadow duration-300 w-1/2">
+                            <CardHeader className="p-0">
+                                <div className="relative h-40">
+                                    {
+                                        match.photos.length === 0 &&
+
+                                        <Image
+                                            src={match.gender === "woman" ? "/img/icons/Woman.jpg" : "/img/icons/Man.jpg"}
+                                            alt={match.gender === "woman" ? "Woman" : "Man"}
+                                            layout="fill"
+                                            objectFit="contain"
+                                            className="rounded-t-lg"
+                                        />
+                                    }
+
+                                    {
+                                        match.photos.length !== 0 &&
+
+                                        <div className="flex flex-start">
+                                        
+                                        {
+                                            match.photos.map((photo: string) =>
+                                            {
+                                                return (
+                                                    <CldImage
+                                                        src={photo}
+                                                        alt={photo}
+                                                        width="150"
+                                                        height="150"
+                                                        crop={{
+                                                            type: "auto",
+                                                            source: true
+                                                        }}
+                                                        className="mr-5"
+                                                    />
+                                                )
+                                            })
+                                        }
+
+                                        </div>
+                                    }
+                                </div>
+                            </CardHeader>
+                            <CardContent className="p-4">
+                                <CardTitle className="text-lg font-semibold text-pink-600 mb-2">
+                                    {match.name}
+                                </CardTitle>
+                                <div className="space-y-2">
+                                    <div className="flex items-center text-sm text-gray-600">
+                                        <CalendarDays className="mr-2 h-4 w-4 text-pink-500" />
+                                        <span>{calculateAge(match.birthdate)} ans</span>
                                     </div>
-                                    <div className="flex items-center">
-                                        <CalendarDays className="w-5 h-5 text-pink-600 mr-2" />
-                                        <span className="font-semibold">Age:</span>
-                                        <span className="ml-2">{calculateAge(m.birthdate)} ans</span>
+                                    <div className="flex items-center text-sm text-gray-600">
+                                        <MapPin className="mr-2 h-4 w-4 text-pink-500" />
+                                        <span>{match.city}</span>
                                     </div>
-                                    <div className="flex items-center">
-                                        <MapPin className="w-5 h-5 text-pink-600 mr-2" />
-                                        <span className="font-semibold">Ville:</span>
-                                        <span className="ml-2">{m.city}</span>
-                                    </div>
-                                    <div className="flex items-center">
-                                        <Globe className="w-5 h-5 text-pink-600 mr-2" />
-                                        <span className="font-semibold">Pays:</span>
-                                        <span className="ml-2">{m.country}</span>
-                                    </div>
+
+                                    <div className="flex items-center justify-around">
+                                        <Button 
+                                            onClick={() =>
+                                                {}
+                                            }
+                                            className="bg-pink-600 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"
+                                        >
+                                            Envoyer un message
+                                        </Button>
+
+                                        <Button 
+                                            onClick={() =>
+                                                {}
+                                            }
+                                            className="bg-pink-600 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"
+                                        >
+                                            Voir son profil
+                                        </Button>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </CardContent>
+                        </Card>
                     )
                 })
             }
+            </div>
         </>
     )
 }
